6a65544dac70870d99f08ff193b9f2fa713c6df1
lkhaajav <lkhaching@gmail.com>
2026-01-11 22:33:12 +0900

    keep doing

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -5,9 +5,11 @@ document.addEventListener("DOMContentLoaded", function() {
 var homeApp = {
 
 	pv : null,
+	table : null,
 
 	originData : null,
 	shuffledData : null,
+	finishedData : [],
 	shuffledIndex : 0,
 
 	action : 0, // 0:start, 1:pause, 2:stop
@@ -16,12 +18,13 @@ var homeApp = {
 
 		this.pv = pv;
 
+		this.initTable();
+
 		this.selectVocDic();
 
 		const btnStart = document.getElementById(`btnStart${this.pv}`);
 		const btnPause = document.getElementById(`btnPause${this.pv}`);
 		const btnStop = document.getElementById(`btnStop${this.pv}`);
-		const btnCheck = document.getElementById(`btnCheck${this.pv}`);
 
 		btnStart.addEventListener("click", function() {
             homeApp.waitForData(() => {
@@ -29,11 +32,13 @@ var homeApp = {
 					homeApp.action = 0;
 					homeApp.myTask();
 				} else if(homeApp.action == 2) {
+					homeApp.finishedData = [];
 					homeApp.shuffle();
 					homeApp.shuffledIndex = 0;
 					homeApp.action = 0;
 					homeApp.myTask();
 				} else {
+					homeApp.finishedData = [];
 					homeApp.shuffle();
 					homeApp.shuffledIndex = 0;
 					homeApp.action = 0;
@@ -121,8 +126,56 @@ var homeApp = {
 		}
 		regDate.textContent = homeApp.shuffledData[i].reg_date;
 
+		homeApp.finishedData.push(homeApp.shuffledData[i]);
+		homeApp.table.setData(homeApp.finishedData);
+
 		homeApp.shuffledIndex++;
 
         setTimeout(homeApp.myTask, second);
+    },
+
+	initTable : function() {
+
+        this.table = new Tabulator(`#table${this.pv}`, {
+            selectableRows:1,
+            layout:"fitColumns",      //fit columns to width of table
+            responsiveLayout:"hide",  //hide columns that don't fit on the table
+            addRowPos:"top",          //when adding a new row, add it to the top of the table
+            history:true,             //allow undo and redo actions on the table
+            pagination:"local",       //paginate the data
+            paginationSize:32,         //allow 7 rows per page of data
+            paginationCounter:"rows", //display count of paginated rows in footer
+            movableColumns:true,      //allow column order to be changed
+            initialSort:[             //set the initial sort order of the data
+                {column:"reg_date", dir:"desc"},
+            ],
+            columnDefaults:{
+                tooltip:true,         //show tool tips on cells
+            },
+            columns:[                 //define the table columns
+                {
+                    formatter: "rowSelection", 
+                    //titleFormatter: "rowSelection", 
+                    hozAlign: "center", // Horizontal alignment of cell contents
+                    headerSort: false,  // Disable sorting on this column
+                    frozen: true,        // Freeze the column in place
+                    headerHozAlign: "center",
+                    width: 60,
+                },
+                {title:"ID", field:"dic_id", width: 120, },
+                {title:"English", field:"eng_word", },
+                {title:"Mongolian", field:"mon_word", },
+                {title:"Date", field:"reg_date", width: 120, },
+            ],
+        });
+
+        this.table.on("tableBuilt", function(){ });
+
+        this.table.on("rowClick", function(e, row){ });
+
+        this.table.on("rowSelected", function(row){ });
+
+        this.table.on("rowDeselected", function(row){ });
+
     },
 }
